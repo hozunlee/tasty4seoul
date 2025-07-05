e8380046e4b2faf15c14aa9153988c4d4be6f14f
hozunlee <ho2yahh@gmail.com>
2025-07-05 14:40:12 +0900

    fix : for deploy

--- src/shared/ui/layout/components/Nav.tsx
+++ src/shared/ui/layout/components/Nav.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect, useState, useMemo } from "react"
+import React, { useMemo } from "react"
 import { motion } from "framer-motion"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
