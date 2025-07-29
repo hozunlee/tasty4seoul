b1d326b10e4b4cba56bc6f1720268366104babdb
hozunlee <ho2yahh@gmail.com>
2025-07-29 15:40:37 +0900

    fix : 주석 처리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Layout } from "@/shared/ui/layout/Layout";
-import Script from "next/script";
+// import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
